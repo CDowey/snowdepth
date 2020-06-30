8c4758de6986120c9cad4c2485c8a0488b282078
Colin Dowey <CDowey@Colins-MacBook-Pro.local>
2020-06-30 18:16:14 -0400

    current snow api req started

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -9,80 +9,88 @@ import ReactDOMServer from 'react-dom/server';
 
 const LeafletMap = () => {
 
-        const [markers, setMarkers] = useState([]);
+    const [markers, setMarkers] = useState([]);
 
-        const bound_style = () => {
-            return {
-                weight: 2,
-                opacity: 1,
-                color: 'black',
-                fillOpacity: 0
-            };
+    const bound_style = () => {
+        return {
+            weight: 2,
+            opacity: 1,
+            color: 'black',
+            fillOpacity: 0
+        };
+
+    }
+    useEffect(() => {
+        //Set Marker Locations
+        const locations = []
+        for (let [key, value] of Object.entries(Stations)) {
+            locations.push([parseFloat(Stations[key].lat), parseFloat(Stations[key].lon)])
+        }
+
+        setMarkers(locations)
+
+        //Example url
+       // https://www.ncei.noaa.gov/access/services/data/v1?dataset=global-summary-of-the-year&dataTypes=DP01,DP05,DP10,DSND,DSNW,DT00,DT32,DX32,DX70,DX90,SNOW,PRCP&stations=ASN00084027&startDate=1952-01-01&endDate=1970-12-31&includeAttributes=true&format=json
+//        var url = new URL("https://geo.example.org/api"),
+//        params = {lat:35.696233, long:139.570431}
+//    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+//    fetch(url).then(...)
+
+
+        //API call to get the current snowdepth for all the stations
+        const getCurrentDepths = async() =>{
 
         }
-        useEffect(() => {
-            const locations = []
-            for (let [key, value] of Object.entries(Stations)) {
-                locations.push([parseFloat(Stations[key].lat), parseFloat(Stations[key].lon)])
-            }
+
+    }, []);
+
+    // This allows the marker to be dynamic, perhaps reflecting the latest measurement at the site?
+    // Would be a lot of requests to get that info unless it is available a different way
+    const icon = L.divIcon({
+        className: 'custom-icon',
+        iconAnchor: [10, 10],
+        html: ReactDOMServer.renderToString(< MapIcon depth={10}
+        />)
+    });
 
 
-            setMarkers(locations)
+    // const isLoading = markerLocations.length === 0
 
-        }, []);
-
-        // This allows the marker to be dynamic, perhaps reflecting the latest measurement at the site?
-        // Would be a lot of requests to get that info unless it is available a different way
-        const icon = L.divIcon({
-                className: 'custom-icon',
-                iconAnchor: [10, 10],
-                html: ReactDOMServer.renderToString( < MapIcon depth = { 10 }
-                    />)
-                });
-
-
-            // const isLoading = markerLocations.length === 0
-
-            return ( <
-                div className = 'MapContainer' >
-                <
-                div className = 'MapHeader' >
-                Current Snowdepths.Click to see comparison with past seasons. <
-                /div> <
-                Map center = {
-                    [43.89, -72.5] }
-                zoom = { 8 }
-                maxZoom = { 9 }
-                zoomControl = { false }
-                attributionControl = { false } > {
+    return (
+        <div className='MapContainer' >
+            <div className='MapHeader' >
+                Current Snowdepths.Click to see comparison with past seasons.
+            </div>
+            <Map center={
+                [43.89, -72.5]}
+                zoom={8}
+                maxZoom={9}
+                zoomControl={false}
+                attributionControl={false} > {
                     markers.map((position, idx) =>
-                        <
-                        Marker key = { `marker-${idx}` }
-                        position = { position }
-                        icon = { icon } >
-                        <
-                        Popup >
-                        <
-                        span > Test Pop up < br / > Easily customizable. < /span> <
-                        /Popup> <
-                        /Marker>
+                        <Marker key={`marker-${idx}`}
+                            position={position}
+                            icon={icon} >
+                            <Popup >
+                                <span > Test Pop up < br /> Easily customizable. </span>
+                            </Popup>
+                        </Marker>
                     )
-                } <
-                GeoJSON key = '1'
-                data = { VT_Boundary }
-                style = { bound_style }
-                /> <
-                TileLayer url = "https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}{r}.png"
-                attribution = '&copy; Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' /
-                >
-                <
-                /Map> <
-                /div>
+                }
+                <GeoJSON
+                    key='1'
+                    data={VT_Boundary}
+                    style={bound_style}
+                />
+                {/* <TileLayer
+                    url="https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}{r}.png"
+                    attribution='&copy; Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' />
+                     */}
+            </Map>
+        </div>
 
+    );
 
+}
 
-            );
-
-        }
-
-        export default LeafletMap;
+export default LeafletMap;
